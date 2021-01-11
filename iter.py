c05eb56d9e7a50338ecbc19b2e20a95f43d9da18
devitos <71663225+devitos@users.noreply.github.com>
2021-01-11 16:19:21 +0300

    Add files via upload

--- iter.py
+++ iter.py
@@ -8,16 +8,31 @@ def create_urlfile():
     beta_file = response.json()
     # Не понял почему к этому файлу нельзя применить команду json.load.
     # json.load(beta_file)
-    wiki = 'https://en.wikipedia.org/wiki/'
-    countries = (country['name']['official'] for country in beta_file)
+    wiki_en = 'https://en.wikipedia.org/wiki/'
+    wiki_ru = 'https://ru.wikipedia.org/wiki/'
+
+    countries = (country for country in beta_file)
 
     with open('wiki_country.txt', 'w', encoding='UTF-8') as f:
         for country in countries:
-            country_url = wiki + country.replace(' ', '_')
-            f.write(f'{country}: {country_url}\n')
-            a[country] = country_url
+            ru_name = country['translations']['rus']['official']
+            en_name = country['name']['official']
+            country_url_en = wiki_en + en_name.replace(' ', '_')
+            country_url_ru = wiki_ru + ru_name.replace(' ', '_')
+            f.write(f'{ru_name}: \n{country_url_en} \n{country_url_ru} \n')
+            a[ru_name] = country_url_en + ' '
+            a[ru_name] += country_url_ru
     pprint.pprint(a)
     return
 
 
 create_urlfile()
+
+# import requests
+# import pprint
+#
+# response = requests.get('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
+# beta_file = response.json()
+# countries = (country['translations']['rus']['official'] for country in beta_file)
+# for country in countries:
+#     pprint.pprint(country)
